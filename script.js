bfd363fa6a25375981ae526b113a283bfbb31f38
Greshnickl <grisha-kushnir@mail.ru>
2023-05-12 20:25:35 +0300

    upgrades+buttons

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 let scoreDiv = document.getElementById("score");
 let clickButton = document.getElementById("clickButton");
-let upgrades = document.getElementsByClassName("upgrades");
+let upgrades_div = document.getElementsByClassName("upgrades");
 let upgradescost = document.getElementsByClassName("upgradescost");
+let upgradeButtons = document.getElementsByClassName("upgradeButton");
+console.log(upgradeButtons[0].tabIndex)
 let score = parseFloat(scoreDiv.innerText);
 let scorePerClick = 1;
 let scorePerSec = 1;
-let TPS = 5;
 clickButton.innerText = "+" + scorePerClick;
+let TPS = 5;
+let upgrades = [[], []];
+upgrades[0] = [0, "Pickaxe", 1, 10, 0, 1, 1.7, (upgrades[0][2]*upgrades[0][4]*upgrades[0][5]), "Pickaxe"];
+upgrades[1] = [1, "Miner", 1, 25, 0, 1, 1.3, (upgrades[0][2]*upgrades[0][4]*upgrades[0][5]), "Miner"];
+upgrades[2] = [1, "Excavator", 1, 100, 0, 1, 1.5, (upgrades[0][2]*upgrades[0][4]*upgrades[0][5]), "Excavator"];
+
+//console.log(upgrades);
+function start(){
+
+}
 function Auto() {
 	score = (parseFloat(score) + (scorePerSec / TPS)).toFixed(2);
 	
@@ -25,7 +36,7 @@ function Click() {
 function scorePlusMod(id, type){
 	if (type === 0) {
 		if (score >= parseInt(upgradescost[id].innerText)){
-			scorePerClick += parseInt(upgrades[id].innerText);
+			scorePerClick += parseInt(upgrades_div[id].innerText);
 			score = (score - parseInt(upgradescost[id].innerText)).toFixed(2);
 			upgradescost[id].innerText = Math.ceil(parseFloat(upgradescost[id].innerText) * 1.7);
 			scoreDiv.innerText = score;
@@ -33,7 +44,7 @@ function scorePlusMod(id, type){
 		}
 	} else if (type === 1) {
 		if (score >= parseInt(upgradescost[id].innerText)){
-			scorePerSec += parseInt(upgrades[id].innerText);
+			scorePerSec += parseInt(upgrades_div[id].innerText);
 			score = (score - parseInt(upgradescost[id].innerText)).toFixed(2);
 			upgradescost[id].innerText = Math.ceil(parseFloat(upgradescost[id].innerText) * 1.7)
 			scoreDiv.innerText = score;
