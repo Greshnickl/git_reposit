916ffe3604a15e1c15a8e527809bf73f6e24ae98
Greshnickl <grisha-kushnir@mail.ru>
2023-05-15 14:11:39 +0300

    loops + upgrades

--- script.js
+++ script.js
@@ -1,56 +1,54 @@
-let scoreDiv = document.getElementById("score");
-let clickButton = document.getElementById("clickButton");
+let goldoreDiv = document.getElementById("goldore");
 let upgrades_div = document.getElementsByClassName("upgrades");
 let upgradescost = document.getElementsByClassName("upgradescost");
 let upgradeButtons = document.getElementsByClassName("upgradeButton");
-console.log(upgradeButtons[0].tabIndex)
-let score = parseFloat(scoreDiv.innerText);
-let scorePerClick = 1;
-let scorePerSec = 1;
-clickButton.innerText = "+" + scorePerClick;
+let upgradenames = document.getElementsByClassName("upgradename");
+let scales = document.getElementsByClassName("scales");
+let upgradedescription = document.getElementsByClassName("upgradedescription");
+let goldore = 10;
+goldoreDiv.innerText = goldore;
 let TPS = 5;
 let upgrades = [[], []];
-upgrades[0] = [0, "Pickaxe", 1, 10, 0, 1, 1.7, (upgrades[0][2]*upgrades[0][4]*upgrades[0][5]), "Pickaxe"];
-upgrades[1] = [1, "Miner", 1, 25, 0, 1, 1.3, (upgrades[0][2]*upgrades[0][4]*upgrades[0][5]), "Miner"];
-upgrades[2] = [1, "Excavator", 1, 100, 0, 1, 1.5, (upgrades[0][2]*upgrades[0][4]*upgrades[0][5]), "Excavator"];
-
+upgrades[0] = ["Pickaxe", 1, 10, 1, 1, 1.1, "Pickaxe", 100];
+upgrades[1] = ["Miner", 5, 25, 0, 1, 1.3, "Miner", 300];
+upgrades[2] = ["Excavator", 7, 50, 0, 1, 1.5, "Excavator", 500];
+upgrades[3] = ["Drill", 11, 100, 0, 1, 1.7, "Drill", 700];
+upgrades[4] = ["Quarry", 13, 150, 0, 1, 1.9, "Quarry", 900];
 //console.log(upgrades);
 function start(){
-
+	goldoreDiv.innerText = goldore;
+	for (var i = 0; i<=upgradenames.length; i++){
+		upgradenames[i].innerText = upgrades[i][0] + " Cost: " + upgrades[i][2] + " gold ore";
+		if (upgrades[i][3] == 0) {
+			upgradedescription[i].innerText = "+ " + upgrades[i][1]*upgrades[i][4] + " gold ore per cycle";
+		} else {
+			upgradedescription[i].innerText = "+ " + upgrades[i][1]*upgrades[i][3]*upgrades[i][4] + " gold ore per cycle";
+		}
+	}
 }
 function Auto() {
-	score = (parseFloat(score) + (scorePerSec / TPS)).toFixed(2);
-	
-	if (score*1000 % 100 == 0) {
-		score = parseFloat(score).toFixed(1);
-	}
-	if (score*1000 % 1000 == 0) {
-		score = parseFloat(score).toFixed(0);
-	}
-	scoreDiv.innerText = score;
+
 }
-function Click() {
-	score = parseFloat(score) + scorePerClick;
-	scoreDiv.innerText = score;
+function scaleup(i){
+	if (goldore >= upgrades[i][2]){
+		goldore -= upgrades[i][2];
+		upgrades[i][3]++;
+		upgrades[i][2] = Math.ceil(upgrades[i][2]*upgrades[i][5]);
+
+		start();
+	}
 }
-function scorePlusMod(id, type){
-	if (type === 0) {
-		if (score >= parseInt(upgradescost[id].innerText)){
-			scorePerClick += parseInt(upgrades_div[id].innerText);
-			score = (score - parseInt(upgradescost[id].innerText)).toFixed(2);
-			upgradescost[id].innerText = Math.ceil(parseFloat(upgradescost[id].innerText) * 1.7);
-			scoreDiv.innerText = score;
-			clickButton.innerText = "+" + scorePerClick;
-		}
-	} else if (type === 1) {
-		if (score >= parseInt(upgradescost[id].innerText)){
-			scorePerSec += parseInt(upgrades_div[id].innerText);
-			score = (score - parseInt(upgradescost[id].innerText)).toFixed(2);
-			upgradescost[id].innerText = Math.ceil(parseFloat(upgradescost[id].innerText) * 1.7)
-			scoreDiv.innerText = score;
+function scale() {
+	for (var i = 0; i<=scales.length; i++){
+		if (upgrades[i][3] !== 0){
+			scales[i].value += 1;
+			if (scales[i].value == scales[i].max) {
+				scales[i].value = 0;
+				goldore += upgrades[i][1]*upgrades[i][3]*upgrades[i][4];
+				goldoreDiv.innerText = goldore;	
+			}
 		}
 	}
-	console.log("Click", scorePerClick);
-	console.log("Per Sec", scorePerSec);
 }
-setInterval(Auto, 1000/TPS);
+setInterval(scale, 10);
+//setInterval(Auto, 1000/TPS);
