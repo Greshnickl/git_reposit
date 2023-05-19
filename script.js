73fa6b1aac3b846a771368f320fd8bee9e099ec3
Greshnickl <grisha-kushnir@mail.ru>
2023-05-19 23:11:13 +0300

    upgrade table +modups_func, remake craft

--- script.js
+++ script.js
@@ -5,35 +5,50 @@ let upgradeButtons = document.getElementsByClassName("upgradeButton");
 let upgradenames = document.getElementsByClassName("upgradename");
 let scales = document.getElementsByClassName("scales");
 let upgradedescription = document.getElementsByClassName("upgradedescription");
-let craftscales = document.getElementsByClassName("craftscales");
-let craftButtons = document.getElementsByClassName("craftButton");
-let craftlabels = document.getElementsByClassName("craftlabel");
 let upgradetable = document.getElementById("upgradetable");
+let craftul = document.getElementById("craftul");
 //let goldore = 110;
 let resurses = [[], []];
-resurses[0] = ["Gold ore", 0, 0, 1, 1000, false];
-resurses[1] = ["Gold nugget", 100, 15, 110, 0, false];
-resurses[2] = ["Gold ignot", 1000, 30, 1100, 0, false];	
+resurses[0] = ["Gold ore", 0, 0, 1, 10, false, 0]; //name, craft price, time to craft, price, count, crafting, from what is crafted
+resurses[1] = ["Gold nugget", 100, 15, 150, 0, false, 0];
+resurses[2] = ["Gold ignot", 250, 30, 500, 0, false, 0];
+resurses[3] = ["Gold ring", 5, 60, 1500, 0, false, 1];	
 let upgrades = [[], []];
 upgrades[0] = ["Pickaxe", 1, 10, 0, 1, 1.1, "Pickaxe", 100];
 upgrades[1] = ["Miner", 5, 25, 0, 1, 1.3, "Miner", 300];
 upgrades[2] = ["Excavator", 7, 50, 0, 1, 1.5, "Excavator", 500];
 upgrades[3] = ["Drill", 11, 100, 0, 1, 1.7, "Drill", 700];
 upgrades[4] = ["Quarry", 13, 150, 0, 1, 1.9, "Quarry", 900];
-let modups = [[[], []], [[], []], [[], []]];
+let modups = [[[], []], [[], []], [[], []], [[], []], [[]]];
 modups[0][0] = [100, 1.5, 25, 0]; //price, mod, pricemod, resurse type need
 modups[0][1] = [1000, 2, 50, 0];
 modups[0][2] = [5000, 2.5, 25, 0];
+modups[0][3] = [10, 3, 5, 1];
 modups[1][0] = [2500, 1.5, 20, 0];
 modups[1][1] = [5000, 2, 25, 0];
 modups[1][2] = [10000, 2.5, 50, 0];
+modups[1][3] = [25, 3, 5, 1];
 modups[2][0] = [5000, 1.5, 25, 0];
 modups[2][1] = [10000, 2, 50, 0];
 modups[2][2] = [25000, 2.5, 100, 0];
+modups[2][3] = [50, 3, 5, 1];
+modups[3][0] = [10000, 1.5, 25, 0];
+modups[3][1] = [25000, 2, 50, 0];
+modups[3][2] = [50000, 2.5, 25, 0];
+modups[3][3] = [100, 3, 5, 1];
+modups[4][0] = [50000, 1.5, 25, 0];
+modups[4][1] = [100000, 2, 50, 0];
+modups[4][2] = [250000, 2.5, 25, 0];
+modups[4][3] = [250, 3, 5, 1];
 //console.log(upgrades);
 function start(){
-	for (var i = 0; i < craftlabels.length; i++){
-		craftlabels[i].innerText = "Craft " + resurses[i+1][0];
+	for (var i = 0; i < craftul.children.length; i++){
+		craftul.children[i].children[0].innerText = "Craft " + resurses[i+1][0].toLowerCase() + ":";
+		craftul.children[i].children[1].max = resurses[i+1][2]*100;
+		craftul.children[i].children[2].innerText = resurses[i+1][1] + " " + resurses[resurses[i+1][6]][0].toLowerCase() + " = 1 " + resurses[i+1][0].toLowerCase();
+	}
+	for (var i = 0; i < upgrades.length; i++){
+		scales[i].max = upgrades[i][7];
 	}
 	for (var i = 0; i < resursesDiv.length; i++){
 		resursesDiv[i].innerText = resurses[i][4];
@@ -72,18 +87,6 @@ function scaleup(i){
 	}
 }
 function scale() {
-	for (var i = 1; i < resurses.length; i++) {
-		if (resurses[i][5] == true) {
-			if (craftscales[i-1].value != craftscales[i-1].max){
-				craftscales[i-1].value++;
-			} else {
-				resurses[i][5] = false;
-				resurses[i][4]++;
-				craftscales[i-1].value = 0;
-				start();
-			}
-		}
-	}
 	for (var i = 0; i<scales.length; i++){
 		if (upgrades[i][3] !== 0){
 			scales[i].value += 1;
@@ -95,14 +98,8 @@ function scale() {
 		}
 	}
 }
-function craft(id) {
-	if (resurses[id][5] != true) {
-		if (resurses[0][4] >= resurses[id][1]){
-			resurses[id][5] = true;
-			resurses[0][4] -= resurses[id][1];
-			start();
-		}
-	}
+function craft() {
+	
 }
 function table() {
 	for (let i = 0; i < modups.length; i++) {
@@ -144,7 +141,10 @@ function table() {
 }
 function modups_func(i, o){
 	if (resurses[modups[i][o][3]][4] >= modups[i][o][0]){
-		
+		resurses[modups[i][o][3]][4] -= modups[i][o][0];
+		modups[i][o][0] *= modups[i][o][2];
+		upgrades[i][4] *= modups[i][o][1];
 	}
+	start();
 }
 setInterval(scale, 10);
